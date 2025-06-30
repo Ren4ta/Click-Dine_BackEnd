8816645c1ad101a059e9061385e7e816468b4ceb
Android Studio <devandroid@ort>
2025-06-30 10:30:05 -0300

    arreglos

--- src/index.js
+++ src/index.js
@@ -2,11 +2,20 @@ import config from './configs/db_config.js'
 import pkg from 'pg'
 const { Client } = pkg;
 
-const client = new Client(config)
-await client.connect();
-let sql = `SELECT * FROM restaurante]`;
-let result = await client.query(sql);
-await client.end();
-console.log(result.rows)
+console.log(config)
+const client = new Client(config);
+try{
+    await client.connect();
+    let sql = `SELECT * FROM restaurante`;
+    let result = await client.query(sql);
+    
+    console.log(result.rows)
+} catch (ex) {
+    console.log(ex)
+} finally {
+    await client.end();
+}
+
+
 
 
